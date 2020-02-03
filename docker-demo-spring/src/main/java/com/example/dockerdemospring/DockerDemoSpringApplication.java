a030de8aa569ede538701e3fdd4c3a9dab1a6caa
Rajat-Mohanty <60385023+Rajat-Mohanty@users.noreply.github.com>
2020-02-03 19:03:05 +0530

    Adding code to master branch.

---
+++ docker-demo-spring/src/main/java/com/example/dockerdemospring/DockerDemoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoSpringApplication.class, args);
+	}
+
+}
---
+++ docker-demo-spring/src/test/java/com/example/dockerdemospring/DockerDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdemospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
